7a957d78593b033b27baab3602089d278db5ed58
Dan Baechtold <latebloomertech@gmail.com>
2021-12-15 17:19:29 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Dan is the champion";
+
+document.body.append(newHeader);
+
+
+
